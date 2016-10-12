17bf3f56f81ce1e9c476d959448f905ac55d4c97
KikePuma <kikefontanlorenzo@gmail.com>
2016-10-12 15:51:13 +0200

    v1.1.1

---
+++ EmoMetro/Data.vb
@@ -0,0 +1,20 @@
+﻿Module Data
+    ''' <summary>
+    '''  @ Kike 'Puma' Fontán
+    '''  https://github.com/KikePuma/
+    ''' </summary>
+
+    ''' ## STYLE VARIABLES ##
+    Public DarkTheme As Boolean = True
+    Public CurrentTheme As Integer = MetroFramework.MetroThemeStyle.Dark
+
+    ''' ## PROGRAM PATH ##
+    Public NullPath As String = "\dev"
+    Public PraatPath As String = My.Application.Info.DirectoryPath.ToString + "\xtern_\praat.exe"
+    Public EEGWriterPath As String = My.Application.Info.DirectoryPath.ToString + "\xtern_\eegwriter\eegwriter.exe"
+    Public ElanPath As String = NullPath
+
+    ''' ### MESSAGES ###
+    Public Msg As String = "\dev"
+
+End Module
---
+++ EmoMetro/EmoApp.vb
@@ -0,0 +1,72 @@
+﻿Public Class EmoApp
+    Private Sub EmoApp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub ThemeCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles ThemeCheckBox.CheckedChanged
+        If DarkTheme Then
+            CurrentTheme = MetroFramework.MetroThemeStyle.Light
+            DarkTheme = 0
+        Else
+            CurrentTheme = MetroFramework.MetroThemeStyle.Dark
+            DarkTheme = 1
+        End If
+
+        Me.Theme = CurrentTheme
+        ThemeCheckBox.Theme = CurrentTheme
+        PraatButton.Theme = CurrentTheme
+        ElanButton.Theme = CurrentTheme
+        WebcamButton.Theme = CurrentTheme
+        Me.Refresh()
+
+    End Sub
+
+    Private Sub PraatButton_Click(sender As Object, e As EventArgs) Handles PraatButton.Click
+        Try
+            System.Diagnostics.Process.Start(PraatPath)
+        Catch startError As Exception
+            '''Console.WriteLine(startError.Message)
+            Msg = "No Program Path Specified"
+            MsgForm.Show()
+            OpenFileDialog.ShowDialog()
+            MsgForm.Close()
+            If OpenFileDialog.FileName <> NullPath Then
+                PraatPath = OpenFileDialog.FileName
+                Msg = "SUCCESSFULLY CHANGED!"
+                MsgForm.Show()
+            End If
+            OpenFileDialog.FileName = NullPath
+        End Try
+    End Sub
+
+    Private Sub ElanButton_Click(sender As Object, e As EventArgs) Handles ElanButton.Click
+        Try
+            System.Diagnostics.Process.Start(ElanPath)
+        Catch startError As Exception
+            '''Console.WriteLine(startError.Message)
+            Msg = "No Program Path Specified"
+            MsgForm.Show()
+            OpenFileDialog.ShowDialog()
+            MsgForm.Close()
+            If OpenFileDialog.FileName <> NullPath Then
+                ElanPath = OpenFileDialog.FileName
+                Msg = "SUCCESSFULLY CHANGED!"
+                MsgForm.Show()
+            End If
+            OpenFileDialog.FileName = NullPath
+        End Try
+    End Sub
+
+    Private Sub WebcamButton_Click(sender As Object, e As EventArgs) Handles WebcamButton.Click
+        WebcamForm.Show()
+    End Sub
+
+    Private Sub EmotivButton_Click(sender As Object, e As EventArgs) Handles EmotivButton.Click
+        Try
+            System.Diagnostics.Process.Start(EEGWriterPath)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
